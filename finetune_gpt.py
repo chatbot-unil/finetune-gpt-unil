3dd559817b44e8df42befb2d31cb188f05e3b2db
Denis Gremaud <denis.gremaud@gmail.com>
2023-12-14 13:38:14 +0100

    update finetuning file

--- finetune_gpt.py
+++ finetune_gpt.py
@@ -6,8 +6,8 @@ from openai import OpenAI
 
 parser = argparse.ArgumentParser(description="Fine-tune OpenAI model.")
 parser.add_argument('--epochs', type=int, default=20, help='Number of training epochs')
-parser.add_argument('--training_data_path', type=str, default='data/training_data.jsonl', help='Path to training data')
-parser.add_argument('--validating_data_path', type=str, default='data/validating_data.jsonl', help='Path to validating data')
+parser.add_argument('--training_data', type=str, default='data/training_data.jsonl', help='Path to training data')
+parser.add_argument('--validating_data', type=str, default='data/validating_data.jsonl', help='Path to validating data')
 parser.add_argument('--model', type=str, default='gpt-3.5-turbo-1106', help='Model to fine-tune')
 args = parser.parse_args()
 
@@ -18,13 +18,18 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 # Load the model
 client = OpenAI()
 
+print("Training file: " + args.training_data)
+print("Validating file: " + args.validating_data)
+print("Model: " + args.model)
+print("Epochs: " + str(args.epochs))
+
 file_training = client.files.create(
-  file=open(args.training_data_path, "rb"),
+  file=open(args.training_data, "rb"),
   purpose="fine-tune",
 )
 
 file_validating = client.files.create(
-	file=open(args.validating_data_path, "rb"),
+	file=open(args.validating_data, "rb"),
   	purpose="fine-tune",
 )
 
